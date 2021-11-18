4babc45e855fbf76df83fb2684366ee734a18364
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-18 10:47:15 +0900

    port 로 전송된 form의 데이터 읽기

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ import path from "path";
 const __dirname = path.resolve();
 const app = express();
 
+app.use(express.urlencoded({extended: true})) 
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/templates/index.html"));
 });
 
 app.get("/write", function (req, res) {
-    console.log(res);
   res.sendFile(path.join(__dirname, "/templates/write.html"));
+});
+
+app.post("/add", function (req, res) {
+  res.send("전송완료");
+  console.log(req.body);
 });
 
 app.listen(8080);
