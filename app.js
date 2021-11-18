079bc0d73fdbd7737313b7734dfa1525f6b483c3
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-18 13:55:22 +0900

    mongodb 연결, 게시글 post 기능 구현, 게시글 모두 읽어오기 get 기능 구현

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-import express from "express";
-import path from "path";
-
-const __dirname = path.resolve();
+const express = require("express");
+const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://LiamJeon:roejddl1@cluster0.xauoo.mongodb.net/shopping-app?retryWrites=true&w=majority",
+  function (error, client) {
+    if (error) return console.log(error);
+    db = client.db("shopping-app");
+    app.listen(8080, () => console.log("listening on 8080"));
+  }
+);
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/templates/index.html"));
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/list", function (req, res) {
+  db.collection("post")
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      res.render("list.ejs", { posts: result });
+    });
 });
 
 app.get("/write", function (req, res) {
-  res.sendFile(path.join(__dirname, "/templates/write.html"));
+  res.sendFile(path.join(__dirname, "/views/write.html"));
 });
 
 app.post("/add", function (req, res) {
   res.send("전송완료");
-  console.log(req.body);
-});
+  db.collection('counter').findOne({name: '게시물개수'}, (err, result)=>{
+      const 총게시물개수 = result.totalPost;
 
-app.listen(8080);
+      db.collection("post").insertOne({ _id:총게시물개수, titie:req.body.title, date:req.body.date}, (err, result)=>{
+        console.log("저장완료");
+        db.collection('counter').updateOne({name: '게시물개수'}, { $inc : {totalPost:1}}, (err, result)=>{ //operator 사용
+            if(err) return console.error(err);
+        })
+      });
+  });
+});
